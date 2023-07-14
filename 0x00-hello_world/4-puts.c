a9216ba86fc8479faf9b571cadf56fde284e5368
basetsanaew22 <basetsanabuhsih@gmail.com>
2023-07-14 14:19:25 -0700

    difficult one

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,11 @@
+#include<stdio.h>
+/**
+ * main - this is the main function to print
+ *Return:  Always 0 
+ */
+
 int main(void)
 {
-        puts("\"Programming is like building a multilingual puzzle); return (0);
+        puts("\"Programming is like building a multilingual puzzle"); 
+	return (0); 
 }
