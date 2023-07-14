5cc05495e3b5b1cad88b2c3d4f55333987fee619
basetsanaew22 <basetsanabuhsih@gmail.com>
2023-07-14 14:32:37 -0700

    push harder

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,8 @@
 #include<stdio.h>
 /**
  * main - this is the main function to print
- *Return:  Always 0 
+ *Return: Always 0 
  */
-
 int main(void)
 {
         puts("\"Programming is like building a multilingual puzzle"); 
