bdb7c716c936389cbeecad9cbfe3421f01844e42
basetsanaew22 <basetsanabuhsih@gmail.com>
2023-07-15 08:59:19 -0700

    please please

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
-#include<stdio.h>
 /**
  * main -i this is the main function 
  * 
- * Return: Always 0 
+ * Return: always 0 
  */
+#include<stdio.h> 
 int main(void)
 {
-        puts("Programming is like building a multilingual puzzle"); 
-	return(0); 
+        puts("/"Programming is like building a multilingual puzzle"); 
+	return (0); 
 }
