92c54996ebec04ce93fd719e96ac4f6e69d214e8
basetsanaew22 <basetsanabuhsih@gmail.com>
2023-07-15 11:52:54 -0700

    last try

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,3 +1,8 @@
+#include<stdio.h>
+/**
+ * main - a c program that use puts function to print 
+ * Return: Always 0 
+ */
 int main(void) 
 {
     puts("Programming is like building a multilingual puzzle");
