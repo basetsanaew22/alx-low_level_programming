377b59e82d0bddfa20f0761af2988ceec864c74c
basetsanaew22 <basetsanabuhsih@gmail.com>
2023-07-15 14:59:23 -0700

    alx 4-puts file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,14 +3,13 @@
 /**
  * main - Entry point of the program
  *
- * Description: Prints the string "Programming is like building a multilingual puzzle"
- *              using the puts function.
+ * Description: Prints the string "Programming is like building a multilingual
+ * puzzle" using the puts function.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("Programming is like building a multilingual puzzle");
 	return (0);
 }
-
