6ab810b87c73426ad361b4dc42479af1bcd5f637
basetsanaew22 <basetsanabuhsih@gmail.com>
2023-07-15 09:03:49 -0700

    task four

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
+#include<stdio.h>
 /**
  * main -i this is the main function 
  * 
  * Return: always 0 
  */
-#include<stdio.h> 
 int main(void)
 {
         puts("/"Programming is like building a multilingual puzzle"); 
