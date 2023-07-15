77bfa4705cf46955049efc7a1949aee85013d17c
basetsanaew22 <basetsanabuhsih@gmail.com>
2023-07-15 09:09:01 -0700

    hello world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 /**
- * main -i this is the main function 
- * 
- * Return: always 0 
- */
+* main -i this is the main function 
+*   
+* Return: Always 0 
+*/
 int main(void)
 {
         puts("/"Programming is like building a multilingual puzzle"); 
