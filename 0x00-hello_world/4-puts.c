82c7844204e4c26132bcb7d51e5bb6de82a0d25c
basetsanaew22 <basetsanabuhsih@gmail.com>
2023-07-15 11:20:41 -0700

    difficult time

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 /**
- * main - a c program that use puts function to print
- * Return: Always 0 
+ * main - a c program that use puts function to print 
+ * Return: Always 0
  */
-int main(void)
-{
-        puts("\"Programming is like building a multilingual puzzle"); 
-	return (0); 
-}
+int main(void) 
+{	
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}	
