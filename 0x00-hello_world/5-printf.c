2b286c57e45f364304a4c8fcf2c7d6b73cd3dcbe
basetsanaew22 <basetsanabuhsih@gmail.com>
2023-07-16 04:38:38 -0700

    tough

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - Entry point 
+ * main -Entry point 
  *
- * Return: 0 when successful 
+ * Return:0 when successful 
  */
 int main(void) 
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0); 
-
 } 
