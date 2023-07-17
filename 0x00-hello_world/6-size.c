1419969b19359aab065c9e1728ac9b936967a68c
basetsanaew22 <basetsanabuhsih@gmail.com>
2023-07-17 07:08:48 -0700

    difficult task

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
-  * main - Entry point of the program 
+  * main -Entry point of the program  
   *
   * Return: Always 0
   */
@@ -14,4 +14,4 @@ int main(void)
 	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %zu byte(s)\n", sizeof(float));
 	return (0);
-} 			
+}	
