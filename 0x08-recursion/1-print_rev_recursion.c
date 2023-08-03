d1706fb4a73f81ecf1bb49128733f23ceabd9dca
basetsanaew22 <basetsanabuhsih@gmail.com>
2023-08-03 06:55:49 -0700

    commit

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - Displays a string in reverse.
+ * @s: The string to be printed.
+ *
+ * Return: None (void function).
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
