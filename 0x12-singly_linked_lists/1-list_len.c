8b9bf2418119373f694591f73fc86618d906bcc9
basetsanaew22 <basetsanabuhsih@gmail.com>
2023-08-25 11:48:08 -0700

    task numbertwo

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * list_len - Counts the number of elements in the linked list
+ * @h: Pointer to the list_t linked list
+ *
+ * Return Thge number of elements in the linked list pointed to by 'h'
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t a = 0;
+	
+	for (; h != NULL; h = h->next)
+	{
+		a++;
+	}
+	return (a);
+}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -13,8 +13,8 @@ typedef struct list_s
 {
 	char *str;
 	unsigned int len;
-	struct list-s *next;
-} list-t;
+	struct list_s *next;
+} list_t;
 
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
