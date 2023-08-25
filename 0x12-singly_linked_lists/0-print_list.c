9f0766d70552d19e5379ab5f718ba334038b8142
basetsanaew22 <basetsanabuhsih@gmail.com>
2023-08-25 10:59:45 -0700

    yoh

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_list- all element to be printed in link list
+ * @h: elements of the counts
+ * Return: counts of the nodes on the list
+ *
+ * Description: the function is used to print the data of nodes in linked list
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t a = 0;
+
+	for (; h != NULL; h = h->next)
+	{
+		if (!h->str)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->str);
+		a++;
+	}
+	return (a);
+}
