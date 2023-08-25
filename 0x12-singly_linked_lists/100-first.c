3c9c82c5746d27d6c83a6d555e72c7cd761847b3
basetsanaew22 <basetsanabuhsih@gmail.com>
2023-08-25 14:44:49 -0700

    dont do it

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void f(void) __attribute__ ((constructor));
+/**
+ * f - Displays a message before the main function is executed
+ */
+void f(void)
+{
+	printf("You're beat! and yet, you must allow, \n");
+	printf("I bore my house upon my back!\n");
+}
