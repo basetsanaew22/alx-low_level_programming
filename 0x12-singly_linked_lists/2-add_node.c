96537f5058ba3b780c5ef200305a016106c3ac9b
basetsanaew22 <basetsanabuhsih@gmail.com>
2023-08-25 13:34:32 -0700

    my life

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -6,9 +6,9 @@
 /**
  * add_node - Creates and inserts a new node at the beginning of a linked list
  * @head: Pointer to a pointer to the list_t linked list
- * @str: Strinfg to be stored in the new node
+ * @str: String to be stored in the new node
  *
- * Return: Adress of the new node, or NULL on failure
+ * Return: Address of the new node, or NULL on failure
  */
 
 list_t *add_node(list_t **head, const char *str)
@@ -19,7 +19,7 @@ list_t *add_node(list_t **head, const char *str)
 	for (len = 0; str[len]; len++)
 	{
 	}
-	n = malloc(dizeof(list_t));
+	n = malloc(sizeof(list_t));
 	if (!n)
 		return (NULL);
 	n->str = strdup(str);
