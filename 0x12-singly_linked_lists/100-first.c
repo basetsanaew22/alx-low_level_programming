fe8fdee6ad2b6631eaccc2577562dae0514e32dc
basetsanaew22 <basetsanabuhsih@gmail.com>
2023-08-25 15:33:30 -0700

    dreams and goals

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -6,6 +6,6 @@ void f(void) __attribute__ ((constructor));
  */
 void f(void)
 {
-	printf("You're beat! and yet, you must allow, \n");
+	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
 }
